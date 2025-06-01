6a20507eacdbbfb09ff91eb101a9ba99f67fcc3d
rubenSaro <40474901+rubenSaro@users.noreply.github.com>
2025-05-31 22:14:42 -0400

    plot slice

--- plotSliceTest.m
+++ plotSliceTest.m
@@ -1,7 +1,7 @@
 % user defined
 slice = 0;
-ax = "x";
-% TODO: some errors if the slice selected is outside the mesh bounds
+ax = "y"; %slice perpendicular to AX
+% TODO: error if the slice selected is outside the mesh bounds
 map_slice_dir = dictionary(["x","y","z"],[1,2,3]);
 row = map_slice_dir(ax);
 slice_node_idx = find(abs(o.mesh.node(row,:) - slice) <= 0.5);
@@ -9,7 +9,8 @@ mask = all(ismember(o.mesh.elem,slice_node_idx),1);
 slice_elem = o.mesh.elem(:,mask);
 slice_label = o.mesh.label(mask);
 figure;
-pdeplot3D(o.mesh.node,slice_elem(:,slice_label==1),ColorMapData = data);
+% TODO: error if there is no selected tissue in that slice
+pdeplot3D(o.mesh.node,slice_elem(:,slice_label==4),ColorMapData = data);
 
 %xlim([-4,6])
 %ylim([slice-1,slice+1])
