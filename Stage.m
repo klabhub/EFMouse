96182bd36c042676df7f97681a6377d517c8d38b
rubenSaro <40474901+rubenSaro@users.noreply.github.com>
2025-06-02 21:47:26 -0400

    a space

--- Stage.m
+++ Stage.m
@@ -6,7 +6,7 @@ classdef Stage < double
         MESH (1)   % Meshed - mesh modified to include electrodes/craniotomoies
         EXPORT (2) % Export - Save .pro and .msh files for getdp
         GETDP (3)  % Run GetDP
-        ATLAS (4) % Save results as .nii.gz files for use in atlas based analysis.
+        ATLAS (4)  % Save results as .nii.gz files for use in atlas based analysis.
     end
 
 end
